6e5503ff9854db98d496e0ca535f09ef4806e737
luke miles <luke@lukemil.es>
2023-01-16 16:44:05 -0800

    first working version

--- src/PeepoRenderer.sol
+++ src/PeepoRenderer.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.15;
 
 import "ethfs/IFileStore.sol";
+import "base64/base64.sol";
 
 contract PeepoRenderer {
     address public peepoToken;
@@ -12,7 +13,19 @@ contract PeepoRenderer {
         ethFileStore = _ethFileStore;
     }
 
-    function renderPeepo(uint256 peepoId) public view returns (string memory) {
-        IFileStore(ethFileStore).getFile("peepo-part.svg").read();
+    function renderPeepo(string memory speed, string memory fillColor) public view returns (string memory) {
+        // get first part of svg, missing script and closing tags
+        string memory svg = IFileStore(ethFileStore).getFile("pp-part2.svg").read();
+        // Pass in speed and color as arguments to the script
+        bytes memory js = abi.encodePacked(
+            "e(\"\",\".f\",\"repeat\",",
+            speed,
+            "*Math.random(),0);document.querySelectorAll(\".bodyfill\").forEach((x) => { x.setAttribute(\"fill\",\"",
+            fillColor,
+            "\"); });\n//]]>\n</script>\n</svg>"
+        );
+        svg = string(abi.encodePacked(svg, Base64.encode(js)));
+
+        return string(svg);
     }
 }
--- test/PeepoRenderer.t.sol
+++ test/PeepoRenderer.t.sol
@@ -2,19 +2,21 @@
 pragma solidity ^0.8.15;
 
 import {DSTest} from "ds-test/test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {console} from "forge-std/console.sol";
+import {PeepoRenderer} from "../src/PeepoRenderer.sol";
+import "base64/base64.sol";
 
-contract ContractBTest is Test {
-    uint256 testNumber;
+contract PeepoRendererTest is DSTest {
+    PeepoRenderer internal peepoRenderer;
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+    address public constant FileStoreGoerli = 0x5E348d0975A920E9611F8140f84458998A53af94;
 
     function setUp() public {
-        testNumber = 42;
+        peepoRenderer = new PeepoRenderer(address(0), FileStoreGoerli);
     }
 
-    function testNumberIs42() public {
-        assertEq(testNumber, 42);
-    }
-
-    function testFailSubtract43() public {
-        testNumber -= 43;
+    function testRenderPeepo() public view {
+        console.log("peepoRenderer.renderPeepo(150, #ff0000):", peepoRenderer.renderPeepo("150", "#ff0000"));
     }
 }
